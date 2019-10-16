6f411f0de789378448ad5688a1c8936388639d78
hkirschke <33073460+hkirschke@users.noreply.github.com>
2019-10-16 20:23:01 -0300

    Create processexecuter

---
+++ ProcessExecute/ProcessExecute/ProcessExecuter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessExecute
+{
+  public class ProcessExecuter
+  {
+    private int _timeToKillProc;
+    private Process _process;
+    public int TimeToKillProc { get => _timeToKillProc; set => _timeToKillProc = value; }
+    public Process Process { get => _process; set => _process = value; }
+
+    public void Execute(string pathExec, string strTimeKill)
+    {
+      Process = Process.Start(pathExec);
+      if (int.TryParse(strTimeKill, out int killTime))
+      {
+        Thread.Sleep(killTime);
+        Process.Kill();
+      }
+    }
+    public void Execute(string pathExec)
+    {
+      Process = Process.Start(pathExec);
+    }
+  }
+}
---
+++ ProcessExecute/ProcessExecute/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessExecute
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+
+        Console.WriteLine("Gerando nova thread para execução em paralelo");
+        Console.WriteLine("Thread principal iniciada");
+        Thread.CurrentThread.Name = "Principal - Program.cs - ProcessExecute";
+        Thread t2;
+        ProcessExecuter processExecuter;
+        //if (args.Length > 1)
+        //{
+        //  t2 = new Thread(new ThreadStart(new ProcessExecuter(args[0], args[1]).Execute()));
+          
+        //}
+        //else
+        //{
+        //  t2 = new Thread(new ThreadStart(st)); 
+        //}
+        //t2.Name = "Secundária - ";
+        //t2.Start();
+
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.StackTrace);
+      }
+    }
+  }
+}
