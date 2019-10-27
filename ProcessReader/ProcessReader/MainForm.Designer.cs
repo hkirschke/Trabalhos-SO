aff1c42498431e4c5acc9159f9e349fd83dcf7c2
hkirschke <33073460+hkirschke@users.noreply.github.com>
2019-10-27 18:46:25 -0300

    Interface change

--- ProcessReader/ProcessReader/MainForm.Designer.cs
+++ ProcessReader/ProcessReader/MainForm.Designer.cs
@@ -219,7 +219,9 @@
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
       this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.MaximizeBox = false;
       this.Name = "MainForm";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.tbControl.ResumeLayout(false);
       this.tbProcess.ResumeLayout(false);
       ((System.ComponentModel.ISupportInitialize)(this.dtgProcess)).EndInit();
